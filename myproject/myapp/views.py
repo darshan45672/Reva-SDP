2a11860f77eb6cdf42c2317f7764eba6e7863aff
darshan45672 <drshnbhandary@gmail.com>
2025-05-09 09:57:43 +0530

    done with ini

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, world. You're at the myapp index.")
+    context = {
+        'title': 'Welcome to MyApp',
+        'message': 'This is the index page of myapp.',
+    }
+
+    return render(request, 'index.html', context)
+
+def about(request):
+    return HttpResponse("This is the about page of myapp.")
+
+def contact(request):
+    return HttpResponse("This is the contact page of myapp.")
