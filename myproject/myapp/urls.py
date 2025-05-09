abab8ef87e8a0112967fd2379b21fb0fad010bf1
darshan45672 <drshnbhandary@gmail.com>
2025-05-09 12:11:51 +0530

    done with template inheritance

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',view=views.index, name='index'),
-    path('about/', view=views.about, name='about'),
-    path('contact/', view=views.contact, name='contact'),
+    path('all-lists/', view=views.allLists, name='all-lists'),
+    path('done-lists/', view=views.doneLists, name='done-lists'),
 ]
